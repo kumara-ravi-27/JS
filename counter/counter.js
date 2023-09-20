6e4a9a95af6ec3f4d14f7a3a4614f9346235df43
kumara_ravi_27 <145559569+kumara-ravi-27@users.noreply.github.com>
2023-09-20 21:34:23 +0530

    Update counter.js

--- counter/counter.js
+++ counter/counter.js
@@ -1,1 +1,31 @@
-123
+let count=0;
+
+function decrease(){
+
+  count-=1;
+  document.getElementById('labelValue').innerHTML=count;
+if(count<0){
+  labelValue.style.color='red';
+
+}
+}
+
+function reset(){
+
+  count=0;
+  document.getElementById('labelValue').innerHTML=count;
+if(count==0){
+  labelValue.style.color='skyblue';
+  
+}
+}
+
+function increase(){
+
+  count+=1;
+  document.getElementById('labelValue').innerHTML=count;
+  if(count>0){
+    labelValue.style.color='green';
+    
+  }
+}
