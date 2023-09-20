21872b2663a77fa662c12c886810f5a1e1ee443c
kumara_ravi_27 <145559569+kumara-ravi-27@users.noreply.github.com>
2023-09-20 21:48:34 +0530

    Create shortCuts.js

---
+++ VScode_tricks/shortCuts.js
@@ -0,0 +1,43 @@
+// ! -->emmet +tab will create the html stucture
+
+
+// .my-div   create div with class or id using shorthcut
+// #+id+tab
+
+/*
+section#section-id.section-class    add clss or id to any element using shortcut
+form#form-id
+input.input-class
+*/
+
+/*
+to generate various i/ps
+input:number
+
+button, checkbox, color, date, datetime, datetime-local, email, file, hidden,
+image, month, number, password, radio, range, reset, search, submit, tel, text, time, url, week
+*/
+
+/*to create anchor tag with links
+a[href="http://www.google.com"]{Google}
+a:link
+a:tel  //tele num
+a:mail
+a:blank
+
+*/
+
+/*to create nested elements
+nav>ul>li
+.parent>.child>
+section#section>h1.heading  // not worked
+*/
+
+/* to create multiple elements using shorts 
+div*3
+p*20
+table>tr*3>td*4
+table*2>tr*3>td*4
+
+
+*/
